61e891cad1ccc6cce6ef03323f899857c415f13f
Kirk Schloegel <kschloeg@gmail.com>
2016-06-02 14:06:07 -0500

    Missed ProductInterface

--- build/product/lib/ProductManager.d.ts
+++ build/product/lib/ProductManager.d.ts
@@ -2,7 +2,9 @@ import ProductInterface = require('../model/ProductInterface');
 declare class ProductManager {
     static DEFAULT_PRODUCT_NAME: string;
     static create: (product: ProductInterface, callback: (err: any, product: ProductInterface) => void) => void;
-    static findById: (organization_id: string, callback: (err: any, product: ProductInterface) => void) => void;
+    static findById: (product_id: string, options: {
+        include_inactive: boolean;
+    }, callback: (err: any, product: ProductInterface) => void) => void;
     static update(args: {
         oldProduct: ProductInterface;
         newProduct: ProductInterface;
